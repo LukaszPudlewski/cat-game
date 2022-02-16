671e7db3e05201f442518de2ce6c3ea8b6fe29a1
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-02-16 17:15:40 +0000

    xxx

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+
 const canvas = document.querySelector('canvas');
 
 const c = canvas.getContext('2d');
 
-canvas.width = //innerWidth;
-800
-canvas.height = //innerHeight;
-600
+canvas.width = 1200
+canvas.height = 600
 
 const gravity = 0.5;
 
@@ -36,29 +35,42 @@ class Player {
         this.draw();
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
         this.velocity.y += gravity;
-        else this.velocity.y = 0
+        
     }
 }
 
 class Platform {
-    constructor({x, y}) {
+    constructor({x, y, image}) {
         this.position = {
             x: x,
             y: y
         }
-        this.width = 200
+        this.image = image
+        this.width = image.width
         this.height = 20
+        
     }
 
     draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x , this.position.y , this.width , this.height)
+        //c.fillStyle = 'blue'
+        //c.fillRect(this.position.x , this.position.y , this.width , this.height)
+        c.drawImage(this.image, this.position.x, this.position.y)
     } 
 }
 
+
+
+const image = new Image()
+image.src = 'platformSmallTall.png'
+
 const player = new Player()
-const platforms = [new Platform({x:200, y:300}), 
-new Platform({x:400, y:500})]
+const platforms = [new Platform({x:200, y:300, image}), 
+new Platform({x:400, y:500, image}), new Platform({x:-1, y:580, image}),
+new Platform({x:600, y:180, image}), new Platform({x:1200, y:500, image}),
+new Platform({x:1500, y:100, image}), new Platform({x:1900, y:400, image}),
+new Platform({x:2100, y:580, image}), new Platform({x:2500, y:280, image}),
+new Platform({x:2900, y:350, image}), new Platform({x:3600, y:550, image})]
+
 const keys = {
     right: {
         pressed: false
@@ -67,16 +79,17 @@ const keys = {
         pressed: false
     }
 }
+let scrollOffset = 0
 
 
 const animate = () => {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
-    platforms.forEach((platform) => {
-        platform.draw()
+    c.fillStyle = 'salmon' 
+    c.fillRect(0, 0, canvas.width, canvas.height)
+        platforms.forEach((platform) => {
+        platform.draw()        
     })
-    
+    player.update()
 
     if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5
@@ -88,13 +101,27 @@ const animate = () => {
         if (keys.right.pressed) {
             platforms.forEach((platform) => {
                 platform.position.x -= 5
+                scrollOffset +=5
+                console.log(scrollOffset)
             })
             
         } else if (keys.left.pressed) {
             platforms.forEach((platform) => {
                 platform.position.x += 5
+                scrollOffset -=5
+                
             })
             
+        }
+
+        if (scrollOffset > 36000) {
+            document.getElementById("alert").style.display = "block"
+            document.getElementById("can1").style.display = "none"
+        }
+
+        if (player.position.y > canvas.height && scrollOffset < 36000) {
+            document.getElementById("alertL").style.display = "block"
+            document.getElementById("can1").style.display = "none"
         }
     }
     platforms.forEach((platform) => {
