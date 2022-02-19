0cbb8dadb135ab3bb9bfc3f3db4447d14e302465
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-02-19 01:31:10 +0000

    coins and lava

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Player {
     constructor() {
         this.position = {
             x: 100,
-            y: 400
+            y: 300
         }
 
         this.velocity = {
@@ -121,6 +121,42 @@ class Deco {
     } 
 }
 
+class Gold {
+    constructor({x,y,goldImage}) {
+        this.position = {
+            x: x,
+            y: y
+        }
+        this.image = goldImage
+        this.width = 128.5
+        this.height = 98
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, 128.5, 98)
+    }
+
+    hide() {
+        this.position.y = 1000
+    }
+}
+
+class Lava {
+    constructor({x,y,lavaImage}) {
+        this.position = {
+            x: x,
+            y: y
+        }
+        this.image = lavaImage
+        this.width = 337
+        this.height = 104
+    }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, 337, 104)
+    }
+}
+
+let score = 0
 
 
 const image = new Image()
@@ -135,17 +171,36 @@ catR.src = 'catR.png'
 const catL = new Image()
 catL.src = 'catL.png'
 
+const goldImage = new Image()
+goldImage.src = 'gold.png'
+
+const lavaImage = new Image()
+lavaImage.src = 'lava.png'
+
 const player = new Player()
 
 const platforms = [new Platform({x:200, y:300, image}), 
-new Platform({x:400, y:500, image}), new Platform({x:-1, y:580, image}),
+new Platform({x:400, y:500, image}), new Platform({x:-1, y:550, image}),
 new Platform({x:600, y:180, image}), new Platform({x:1200, y:500, image}),
 new Platform({x:1500, y:100, image}), new Platform({x:1900, y:400, image}),
-new Platform({x:2100, y:580, image}), new Platform({x:2500, y:280, image}),
+new Platform({x:2100, y:550, image}), new Platform({x:2500, y:280, image}),
 new Platform({x:2900, y:350, image}), new Platform({x:3600, y:550, image})]
 
 const deco = [new Deco({x: 600, y: 30, cloud}), new Deco({x: 2000, y: 30, cloud})]
 
+const gold = [new Gold({x: 100, y: 100, goldImage}), new Gold({x: 200, y: 300, goldImage}),
+    new Gold({x: 800, y: 400, goldImage}), new Gold({x: 1200, y: 100, goldImage}),
+    new Gold({x: 1500, y: 200, goldImage}), new Gold({x: 1900, y: 90, goldImage}),]
+
+const lava = [new Lava({x:-1, y: 530, lavaImage}), new Lava({x:-338, y: 530, lavaImage}),
+    new Lava({x:336, y: 530, lavaImage}), new Lava({x:674, y: 530, lavaImage}),
+    new Lava({x:1011, y: 530, lavaImage}), new Lava({x:1348, y: 530, lavaImage}),
+    new Lava({x:1685, y: 530, lavaImage}), new Lava({x:2022, y: 530, lavaImage}),
+    new Lava({x:2359, y: 530, lavaImage}), new Lava({x:2696, y: 530, lavaImage}),
+    new Lava({x:3033, y: 530, lavaImage}), new Lava({x:3370, y: 530, lavaImage}),
+    new Lava({x:3707, y: 530, lavaImage}), new Lava({x:4044, y: 530, lavaImage}),
+    new Lava({x:-675, y: 530, lavaImage}),]
+    
 const keys = {
     right: {
         pressed: false
@@ -161,11 +216,18 @@ const animate = () => {
     requestAnimationFrame(animate)
     c.fillStyle = 'salmon' 
     c.fillRect(0, 0, canvas.width, canvas.height)
+    
+     gold.forEach(gold => {
+        gold.draw()
+    })
     deco.forEach(deco => {
         deco.draw()
     })
-        platforms.forEach((platform) => {
+    platforms.forEach((platform) => {
         platform.draw()        
+    })
+    lava.forEach(lava => {
+        lava.draw()
     })
     player.update()
 
@@ -185,14 +247,27 @@ const animate = () => {
             deco.forEach((deco) => {
                 deco.position.x -= 4
             })
+            gold.forEach((gold) => {
+                gold.position.x -= 5
+            })
+            lava.forEach((lava) => {
+                lava.position.x -= 5
+            })
             
         } else if (keys.left.pressed) {
             platforms.forEach((platform) => {
                 platform.position.x += 5
                 scrollOffset -=5
-                
             })
-            
+            deco.forEach((deco) => {
+                deco.position.x += 4
+            })
+            gold.forEach((gold) => {
+                gold.position.x += 5
+            })
+            lava.forEach((lava) => {
+                lava.position.x += 5
+            })
         }
 
         if (scrollOffset > 35500) {
@@ -221,6 +296,17 @@ const animate = () => {
         player.velocity.y = 0
     } 
 })
+    gold.forEach((gold) => {
+        if (player.position.y + player.height >= gold.position.y &&
+            player.position.x + player.width  - 60 >= gold.position.x &&
+            player.position.x + 60 <= gold.position.x + gold.width &&
+            player.position.y  + 20 <= gold.position.y + gold.height
+             ) {                 
+                 gold.hide()
+                 score++
+                 document.getElementById("score").textContent = "Score: " + score                                   
+             }
+    })
 }
 
 animate();
